0af007d0ec99351e9f132cdee4679f917b774748
zaiky09 <zamilmozamil@outlook.com>
2023-01-13 13:06:01 +0300

    tests passing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,13 @@
 import React from "react";
+import {v4 as uuid} from "uuid";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+    <nav>
+      {links.map((link) => <a key={uuid()} href={`#${link}`}>{link}</a>)}
+    </nav>);
 }
 
 export default NavBar;
